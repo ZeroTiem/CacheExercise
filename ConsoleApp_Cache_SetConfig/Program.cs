86127415ad50d9a4b79cec1372300d9e56f98b36
VS_Online <maxhank22@gmail.com>
2018-01-07 05:16:51 +0800

    設定3 不符合預期

--- ConsoleApp_Cache_SetConfig/Program.cs
+++ ConsoleApp_Cache_SetConfig/Program.cs
@@ -14,7 +14,6 @@ namespace ConsoleApp_Cache_SetConfig
         {
             Console.WriteLine("===ConsoleApp_Cache_SetConfig===");
             ObjectCache cache2 = MemoryCache.Default;
-            Thread.Sleep(1000);//延遲1000ms，也就是1秒
             var physicalMemoryLimit = (cache2 as MemoryCache).PhysicalMemoryLimit;
             Console.WriteLine("physicalMemoryLimit：" + physicalMemoryLimit + "%");
             var cacheMemoryLimit = (cache2 as MemoryCache).CacheMemoryLimit;
